615a0ac721ee1ec5e9bf35054e57421725ac1954
Olexandr Rudyy <rudojsru@gmail.com>
2019-08-16 19:32:33 +0200

    Initial commit

---
+++ src/main/java/com/friend/app/PetHomeApplication.java
@@ -0,0 +1,13 @@
+package com.friend.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetHomeApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/static/app.js
@@ -0,0 +1,26 @@
+app = {
+
+
+
+
+  initDateTimePicker: function() {
+    $('.datetimepicker').datetimepicker({
+      icons: {
+        time: "fa fa-clock-o",
+        date: "fa fa-calendar",
+        up: "fa fa-chevron-up",
+        down: "fa fa-chevron-down",
+        previous: 'fa fa-chevron-left',
+        next: 'fa fa-chevron-right',
+        today: 'fa fa-screenshot',
+        clear: 'fa fa-trash',
+        close: 'fa fa-remove'
+      }
+    });
+  }
+
+
+
+}
+
+app.initDateTimePicker();
